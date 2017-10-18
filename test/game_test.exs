1d1a7c75be2b29548ab46527ce3fe3adc53745ab
James Doyley <jdoyley@gmail.com>
2017-10-18 16:25:36 +0100

    Add test for new game

---
+++ test/game_test.exs
@@ -0,0 +1,14 @@
+defmodule GameTest do
+  use ExUnit.Case
+
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
+    assert Enum.all?(game.letters, fn(x) -> String.match?(x, ~r/[a-z]/) end)
+  end
+end
